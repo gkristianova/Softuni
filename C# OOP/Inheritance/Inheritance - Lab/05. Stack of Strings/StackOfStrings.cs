86cdb005805cfbd49d0f9f4172e6418dc7211e7f
gkristianova <76521541+gkristianova@users.noreply.github.com>
2021-02-23 21:07:10 +0200

    C# OOP

---
+++ C# OOP/Inheritance/Inheritance - Lab/05. Stack of Strings/StackOfStrings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+        public Stack<string> AddRange()
+        {
+            return this;
+        }
+    }
+}
