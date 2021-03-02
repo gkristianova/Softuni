fd371540954c8e3d9b52964beb47493b7a256f74
gkristianova <76521541+gkristianova@users.noreply.github.com>
2021-03-02 17:53:12 +0200

    C# OOP

---
+++ C# OOP/Polymorphism/Polymorphism - Lab/Shapes/Rectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+
+        public Rectangle(double height, double width)
+        {
+            Height = height;
+            Width = width;
+            
+        }
+
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public override double CalculateArea()
+        {
+            return Width * Height;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return (Width + Height) * 2;
+        }
+
+        public override string Draw()
+        {
+            return $"{base.Draw()} {GetType().Name}";
+        }
+    }
+}
